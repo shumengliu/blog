26d8763dc3bd0e81b3026e3bea4744344e787a84
Shumeng <60369045+shumengliu@users.noreply.github.com>
2023-02-08 01:12:29 +0000

    Add more projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,8 +4,6 @@ const projectsData = [
   // TODO Buddy Scheme
   // TODO Note if it is a outside link, start with https://
   // TODO The WHILE language compiler
-  // - [This site](https://github.com/httom123/TeamTiger)
-  // - [Lynx Bank](https://github.com/RoundofThree/lynx)
   // - [Buddy Scheme](https://github.com/httom123/TeamTiger)
   // - KCLCSSA WeChat Mini Program
   {
@@ -13,6 +11,25 @@ const projectsData = [
     description: 'The site you are visiting now',
     imgSrc: '/static/images/projects/nextjs.png',
     href: 'https://github.com/shumengliu/blog',
+  },
+  {
+    title: 'Lynx Bank',
+    description:
+      'A mock banking system with realistic look to delay scammers. Built with Ruby on Rails.',
+    imgSrc: '/static/images/logo.png',
+    href: 'https://github.com/RoundofThree/lynx',
+  },
+  {
+    title: 'BG Simulator',
+    description: 'Simulate Hearthstone Battlegrounds fights using Java with a JavaFX GUI.',
+    imgSrc: '/static/images/logo.png',
+    href: 'https://github.com/shumengliu/BGSimulator',
+  },
+  {
+    title: 'Focus Book',
+    description: 'Block sites on Android using Accessibility.',
+    imgSrc: '/static/images/logo.png',
+    href: 'https://github.com/shumengliu/focus-book',
   },
 ]
 
